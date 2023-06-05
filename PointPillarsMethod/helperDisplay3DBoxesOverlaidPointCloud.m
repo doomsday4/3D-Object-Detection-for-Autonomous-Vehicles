d53e122bb255c332774b3a494d3c6cf84f31a444
doomsday4 <amankhilani.ajmer@gmail.com>
2023-06-05 10:52:44 +0530

    pointPillars

---
+++ PointPillarsMethod/helperDisplay3DBoxesOverlaidPointCloud.m
@@ -0,0 +1,14 @@
+function helperDisplay3DBoxesOverlaidPointCloud(ptCld,labelsCar,carColor,...
+    labelsTruck,truckColor,titleForFigure)
+% Display the point cloud with different colored bounding boxes for different
+% classes.
+    figure;
+    ax = pcshow(ptCld);
+    showShape('cuboid',labelsCar,'Parent',ax,'Opacity',0.1,...
+        'Color',carColor,'LineWidth',0.5);
+    hold on;
+    showShape('cuboid',labelsTruck,'Parent',ax,'Opacity',0.1,...
+        'Color',truckColor,'LineWidth',0.5);
+    title(titleForFigure);
+    zoom(ax,1.5);
+end
